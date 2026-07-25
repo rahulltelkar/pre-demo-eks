b9f6b5df8bcf0783c7eb8cdcad7f854940186b43
rahulltelkar <rahul.friends262@gmail.com>
2026-07-25 05:13:20 +0000

    add initial teraform file stracture

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
