a2c470c5e8d32dbf91a57a3a717057c18cb8b1a3
rahulltelkar <rahul.friends262@gmail.com>
2026-07-25 10:27:31 +0000

    Refactor Terraform and provision production-style EKS cluster

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,5 +1,6 @@
-# AWS Configuration
-aws_region = "ap-south-1"
-
-# Environment
-environment = "dev"
+aws_region           = "ap-south-1"
+environment          = "dev"
+cluster_name         = "platform-demo-eks"
+vpc_cidr             = "10.0.0.0/16"
+public_subnet_1_cidr = "10.0.1.0/24"
+public_subnet_2_cidr = "10.0.2.0/24"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,32 @@ variable "environment" {
   description = "Deployment environment"
   type        = string
 }
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR block for public subnet 1"
+  type        = string
+}
+
+variable "public_subnet_2_cidr" {
+  description = "CIDR block for public subnet 2"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "EKS Cluster Name"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for worker nodes"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "kubernetes_version" {
+  default = "1.33"
+}
